97349a5c922d82b3c18b7eb58677dbc25945da29
fabriciobizotto <fabricio.bizotto@gmail.com>
2021-07-19 10:07:19 -0300

    commit professor

--- pages/contato.php
+++ pages/contato.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Revisae</title>
+    <title>Contato</title>
 
-    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
 </head>
 
 <body>
-    <section><?php include 'pages/menu.php' ?></section>
-    <h1>Página Inicial</h1>
+    <section><?php include 'menu.php' ?></section>
+    <h1>Contato</h1>
 
-    <script src="public/js/bootstrap.min.js"></script>
+    <script src="../public/js/bootstrap.min.js"></script>
 </body>
 
 </html>
---
+++ pages/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Revisae</title>
+
+    <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div>
+        <section><?php include 'menu.php' ?></section>
+
+        <div class="container">
+            <h1>Página Inicial</h1>
+        </div>
+    </div>
+
+    <script src="../public/js/bootstrap.min.js"></script>
+</body>
+
+</html>
